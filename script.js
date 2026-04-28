c71ca5e1058a902ace446cb2a5a7e6f1f56dd295
liz-mar <violet9834@gmail.com>
2026-04-28 02:08:39 -0600

    Cambios finales ropa deportiva

--- script.js
+++ script.js
@@ -4,35 +4,6 @@ function agregarCarrito(){
 
 contador++;
 
-document.getElementById("contador").textContent = contador;
-
 alert("¡Su producto ha sido agregado exitosamente al carrito!");
-
-}
-
-function mostrarCategoria(categoria){
-
-const hombre = document.querySelector(".hombre");
-const mujer = document.querySelector(".mujer");
-
-
-if(categoria==="todo"){
-hombre.classList.remove("oculto");
-mujer.classList.remove("oculto");
-}
-
-
-if(categoria==="hombre"){
-// SOLO mostrar hombre
-hombre.classList.remove("oculto");
-mujer.classList.add("oculto");
-}
-
-
-if(categoria==="mujer"){
-// SOLO mostrar mujer
-mujer.classList.remove("oculto");
-hombre.classList.add("oculto");
-}
 
 }
