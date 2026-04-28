88281b3440bbc78137a0d9965157e5b45511148e
JesusCaballeroBasilio <jesus.caballerobasilio@outlook.com>
2026-04-27 22:37:18 -0600

    Se modico el carrito para que muestre el total, ademas se le dio sangria al archivo ropa.html

--- carrito.js
+++ carrito.js
@@ -7,28 +7,37 @@ function mostrarCarrito() {
   let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
   const contenedor = document.getElementById("lista-carrito");
+  contenedor.innerHTML = "";
 
-  if (carrito.length === 0) {
-    contenedor.innerHTML = "<p>No hay productos en el carrito</p>";
-    return;
-  }
+  let total = 0;
 
   carrito.forEach(producto => {
+    const subtotal = producto.precio * producto.cantidad;
+    total += subtotal;
+
     const item = document.createElement("div");
     item.classList.add("card", "p-3", "mb-3");
 
     item.innerHTML = `
-    <div class="d-flex align-items-center gap-3">
-      <img src="${producto.imagen}" width="80">
-      <div>
-        <h5>${producto.nombre}</h5>
-        <p>$${producto.precio}</p>
+      <div class="d-flex align-items-center gap-3">
+        <img src="${producto.imagen}" width="80">
+        <div>
+          <h5>${producto.nombre}</h5>
+          <p>Precio: $${producto.precio}</p>
+          <p>Cantidad: ${producto.cantidad}</p>
+          <p>Subtotal: $${subtotal}</p>
+        </div>
       </div>
-    </div>
-`;
+    `;
 
     contenedor.appendChild(item);
   });
+
+  // Mostrar total final
+  const totalHTML = document.createElement("h3");
+  totalHTML.textContent = "Total: $" + total;
+
+  contenedor.appendChild(totalHTML);
 }
 
 mostrarCarrito();
--- scripthome.js
+++ scripthome.js
@@ -8,13 +8,34 @@ function verProductos(){
   const mensaje = document.createElement("p");
   mensaje.textContent = product + " agregado al carrito 🛒";
   document.body.appendChild(mensaje);
-}*/
+}
 function agregarCarrito(nombre, precio, imagen) {
+
   let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
   carrito.push({
     nombre: nombre,
     precio: precio,
     imagen: imagen
   });
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+}*/
+
+function agregarCarrito(nombre, precio, imagen) {
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+  // Buscar si ya existe
+  let productoExistente = carrito.find(p => p.nombre === nombre);
+
+  if (productoExistente) {
+    productoExistente.cantidad += 1;
+  } else {
+    carrito.push({
+      nombre: nombre,
+      precio: precio,
+      imagen: imagen,
+      cantidad: 1
+    });
+  }
+
   localStorage.setItem("carrito", JSON.stringify(carrito));
 }
